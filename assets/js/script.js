cfbce71a65944e22830f642cff53d8efe9b9cd34
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2025-10-27 12:53:45 -0300

    feat: Introdução js

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+const { StrictMode } = require("react");
+
+StrictMode
+
+// Comentário de uma linha
+/*
+Comentário
+de várias linhas
+*/
+
+// let nome = "Alexandre"; // string
+
+// var cidade = "São Paulo"; // (não recomendado)
+
+// const PI = 3.14; // PI
+
+// var nr = 20;
+console.log("QUAL é VALOR DE VAR : " + nr);
+
+if(true){
+    var nr = 10;
+}
+
+
