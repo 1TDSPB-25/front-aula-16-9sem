09bb4207061432fc7519fde0d7938da1aad68e0b
GuiDSLeopoldino <guilherme.leopoldino@gmail.com>
2025-10-27 15:24:33 -0300

    ADICIONADO JAVA SCRIPT

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+function soma (){
+      var nr1 = 10
+      var nr2 = 10  
+    console.log("Resultado : " +( nr1 +nr2))
+      }
+
+
+
+const botao = document.getElementById("btnExemplo");
+
+botao.addEventListener("click", function(){
+    soma();
+});
