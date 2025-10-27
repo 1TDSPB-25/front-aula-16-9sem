fc2b053764f09776db75b0849b0a76e376ae7416
GuilhermeAnitelli <guilherme.anitelli10@gmail.com>
2025-10-27 12:09:36 -0300

    adição de js

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+// Comentario de uma linha
+/* Comentario
+de
+muitas
+linhas */
+
+let nome =  "Alexandre"; // String
+
+var cidade = "São Paulo"; // (não recomendado)
+
+const PI = 3.14; // PI
+
+var nr = 20;
+
+if(true){
+    var nr = 10;
+}
+
+console.log("Qual valor de var : " + nr); 
