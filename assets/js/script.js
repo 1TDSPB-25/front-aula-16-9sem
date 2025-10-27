c7aea018215233e9266ca4004be416d1478fdfb8
g-barca <oguilhermebarca@gmail.com>
2025-10-27 11:59:41 -0300

    iniciando com JS

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+  function soma(){
+    var nr1 = 10;
+    var nr2 = 10;
+    console.log("resultado: "+ (nr1 + nr2));
+  }
+
+
+
+  const botao = document.getElementById("btnexemplo");
+
+  botao.addEventListener("click", function(){
+    soma();
+  }); 
+
+
+  /*
+let nome = "Gui" - String
+
+var cidade = "Sp" - (n recomendado)
+
+const pi = 3.1415 - PI
+
+  */
